811362d632e04ab2f106fc2f1571835654a16080
ekamada <ekamada11@gmail.com>
2025-11-06 23:23:35 -0500

    Added nvim-oil plugin, updated default colorscheme, and updated options to use smartcase search

--- init.lua
+++ init.lua
@@ -17,6 +17,7 @@ require("config.lazy")  -- Plugin Manager.
 require("lualine").setup({}) 	-- Statusline
 require("mini.pairs").setup()   -- Automatically close brackets and quotes
 require("nvim-tree").setup()    -- File tree for navigation
+require("oil").setup()          -- Folder Navigation
 require('tiny-inline-diagnostic').setup({	-- Better diagnostic messages
     preset = "powerline",
     throttle = 20,
@@ -36,13 +37,15 @@ require("lazydev").setup() -- Adds Vim API to lua language server
 -- Vim Configuration
 --------------------
 -- vim.cmd.colorscheme("tokyonight") -- Colorscheme
-vim.cmd.colorscheme("rose-pine-main") -- Colorscheme
+vim.cmd.colorscheme("rose-pine-moon") -- Colorscheme
 
-vim.opt.nu = true       -- Numbered Lines
-vim.opt.wrap = false    -- No Text Wrapping
-vim.o.expandtab = true	-- Neovim always uses spaces instead of tabs
-vim.o.tabstop = 4       -- Set Tabstops every 4 spaces
-vim.o.softtabstop = 4   -- Config backspace to jump to the prev. tabstop 
-vim.o.shiftwidth = 4 	-- Set the shift width to 4
-vim.o.textwidth = 80 	-- Text width for formatting text into paragraphs (using gw)
+vim.opt.nu = true           -- Numbered Lines
+vim.opt.wrap = false        -- No Text Wrapping
+vim.o.expandtab = true	    -- Neovim always uses spaces instead of tabs
+vim.o.tabstop = 4           -- Set Tabstops every 4 spaces
+vim.o.softtabstop = 4       -- Config backspace to jump to the prev. tabstop 
+vim.o.shiftwidth = 4 	    -- Set the shift width to 4
+vim.o.textwidth = 80 	    -- Text width for formatting text into paragraphs (using gw)
+vim.o.smartindent = true    -- Smart Indent
+vim.o.smartcase = true      -- ignore case when searching with lowercase letters
 
